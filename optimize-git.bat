b0e8111e0ffb1e5215eac0169ead1818f16d50ec
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-18 04:23:05 -0400

    Create optimize-git.bat

---
+++ optimize-git.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enableextensions
+color 0a
+
+pushd "%~dp0"
+if not "%1"=="max" start /max cmd /d /c %0 max & goto :eof
+set myDir=%cd%
+
+:--------------------------------------------------------------------------------
+
+git.exe clone "https://github.com/slyfox1186/imagemagick-large-file-optimize.git"
+pushd "imagemagick-large-file-optimize"
+move /y "optimize.bat" "%myDir%"
+popd
+rd /s /q "imagemagick-large-file-optimize"
+call "optimize.bat"
