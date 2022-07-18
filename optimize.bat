c01c9db7ea9a793c1ac29f544b9f08109bf4131d
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-18 04:25:20 -0400

    Update optimize.bat

--- optimize.bat
+++ optimize.bat
@@ -2,12 +2,15 @@
 setlocal enableextensions
 color 0a
 
+:--------------------------------------------------------------------------------
+
 pushd "%~dp0"
 if not "%1"=="max" start /max cmd /d /c %0 max & goto :eof
 set myDir=%cd%
 
 :--------------------------------------------------------------------------------
 
+REM RUN OPTIMIZE SCRIPT ON ALL JPG FILES IN THE SCRIPTS DIRECTORY
 git.exe clone "https://github.com/slyfox1186/imagemagick-large-file-optimize.git"
 pushd "imagemagick-large-file-optimize"
 move /y "optimize.bat" "%myDir%"
