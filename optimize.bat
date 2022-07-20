1f7e110b4a48c9dd8d1b3d15c1926ac915334fe8
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-20 07:15:51 -0400

    Update optimize.bat

--- optimize.bat
+++ optimize.bat
@@ -30,13 +30,13 @@ IF EXIST "IMagick_Cache_Files\*.mpc" (GOTO CONVERT) ELSE (MD "IMagick_Cache_File
 REM FIND ALL JPG FILES AND CONVERT THEM TO TEMPORARY CACHE FORMAT
 SETLOCAL ENABLEEXTENSIONS
 FOR %%G IN (*.jpg) DO (
-	FOR /F "TOKENS=1-2" %%H IN ('identify.exe +ping -format "%%w %%h" "%%G"') DO (
-		ECHO Creating: %%~nG.mpc ^+ %%~nG.cache
-		ECHO=
+    FOR /F "TOKENS=1-2" %%H IN ('identify.exe +ping -format "%%w %%h" "%%G"') DO (
+        ECHO Creating: %%~nG.mpc ^+ %%~nG.cache
+        ECHO=
         curl.exe %CONVERT_DL% > "convert.exe"
         powershell.exe -NoL -NoP -W Maximized -Exe Bypass -C "& {Start-Process -FilePath '%CONVERT%' -ArgumentList '-monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -auto-level -enhance -interlace none -colorspace sRGB "IMagick_Cache_Files\%%~nG.mpc"'}"
-		CLS
-	)
+        CLS
+    )
 )
 
 :----------------------------------------------------------------------------------
@@ -45,12 +45,12 @@ REM CONVERT CACHE FILES INTO JPG
 :CONVERT
 SETLOCAL ENABLEEXTENSIONS
 FOR %%G IN ("IMagick_Cache_Files\*.mpc") DO (
-	ECHO Converting: %%~nG.cache ^>^> %%~nG.jpg
-	ECHO=
+    ECHO Converting: %%~nG.cache ^>^> %%~nG.jpg
+    ECHO=
     curl.exe %CONVERT_DL% > "convert.exe"
-	powershell.exe -NoL -NoP -W Maximized -Exe Bypass -C "& {Start-Process -FilePath '%CONVERT%' -ArgumentList ''%%G' -monitor '%%~nG.jpg''}"
-	CLS
-	)
+    powershell.exe -NoL -NoP -W Maximized -Exe Bypass -C "& {Start-Process -FilePath '%CONVERT%' -ArgumentList ''%%G' -monitor '%%~nG.jpg''}"
+    CLS
+    )
 )
 
 :----------------------------------------------------------------------------------
