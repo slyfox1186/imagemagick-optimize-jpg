5a6ed97fcd7b807b180ed984cabdc7d3e91a2794
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-02 21:28:41 -0400

    Update optimize.bat

--- optimize.bat
+++ optimize.bat
@@ -15,8 +15,33 @@ FOR %%A IN (.) DO TITLE %%~nxA
 
 :----------------------------------------------------------------------------------
 
+REM DELETE AND MOVE TEMP FILES
+IF EXIST "urls.txt" DEL /Q "urls.txt"
+
+IF EXIST "convert.exe" IF EXIST "identify.exe" (
+    MOVE /Y "%TMP%\convert.exe"
+    MOVE /Y "%TMP%\identify.exe"
+    CLS
+  ) ELSE (
+    CLS & ECHO AT LEAST ONE FILE IS MISSING: convert.exe or identify.exe
+    ECHO=
+    PAUSE
+    GOTO :EOF
+)
+
+CLS
+IF EXIST "%TMP%\convert.exe" (ECHO convert.exe YES) ELSE (ECHO convert.exe NO)
+ECHO=
+PAUSE
+IF EXIST "%TMP%\identify.exe" (ECHO identify.exe YES) ELSE (ECHO identify.exe NO)
+ECHO=
+PAUSE
+GOTO :EOF
+
+:----------------------------------------------------------------------------------
+
 REM SKIP TO CONVERT IF CACHE FILES ALREADY EXIST OR CREATE THE TEMP DIRECTORY
-IF EXIST "temp-cache-files\*.mpc" (GOTO CONVERT) ELSE (MD temp-cache-files)
+IF EXIST "%TMP%\temp-cache-files\*.mpc" (GOTO CONVERT) ELSE (MD "%TMP%\temp-cache-files")
 
 :----------------------------------------------------------------------------------
 
@@ -28,7 +53,7 @@ FOR %%G IN (*.jpg) DO (
         ECHO=
         convert.exe "%%G" -monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip ^
         -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off ^
-        -auto-level -enhance -interlace none -colorspace sRGB "temp-cache-files\%%~nG.mpc"
+        -auto-level -enhance -interlace none -colorspace sRGB "%TMP%\temp-cache-files\%%~nG.mpc"
         CLS
     )
 )
@@ -38,7 +63,7 @@ FOR %%G IN (*.jpg) DO (
 REM CONVERT CACHE FILES INTO THE OPTIMIZED JPG VERSION
 :CONVERT
 SETLOCAL ENABLEEXTENSIONS
-FOR %%G IN ("temp-cache-files\*.mpc") DO (
+FOR %%G IN ("%TMP%\temp-cache-files\*.mpc") DO (
     ECHO Converting: %%~nG.cache ^>^> %%~nG.jpg
     ECHO=
     convert.exe "%%G" -monitor "%%~nG.jpg"
@@ -48,8 +73,9 @@ FOR %%G IN ("temp-cache-files\*.mpc") DO (
 :----------------------------------------------------------------------------------
 
 REM CLEANUP TEMP FILES AND FOLDERS
-RD /S /Q temp-cache-files
+RD /S /Q %TMP%\temp-cache-files
 DEL /Q convert.exe
+DEL /Q identify.exe
 DEL /Q urls.txt
 START "" /MAX explorer.exe "%CD%"
 START "" /I CMD /D /C DEL /Q optimize.bat
