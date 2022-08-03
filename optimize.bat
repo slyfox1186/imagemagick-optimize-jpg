b2e09ca969580eb1228cc6087171d551d80661fe
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-02 21:39:48 -0400

    Update optimize.bat

--- optimize.bat
+++ optimize.bat
@@ -28,15 +28,6 @@ IF EXIST "convert.exe" IF EXIST "identify.exe" (
     GOTO :EOF
 )
 
-CLS
-IF EXIST "%TMP%\convert.exe" (ECHO convert.exe YES) ELSE (ECHO convert.exe NO)
-ECHO=
-PAUSE
-IF EXIST "%TMP%\identify.exe" (ECHO identify.exe YES) ELSE (ECHO identify.exe NO)
-ECHO=
-PAUSE
-GOTO :EOF
-
 :----------------------------------------------------------------------------------
 
 REM SKIP TO CONVERT IF CACHE FILES ALREADY EXIST OR CREATE THE TEMP DIRECTORY
@@ -47,10 +38,10 @@ IF EXIST "%TMP%\temp-cache-files\*.mpc" (GOTO CONVERT) ELSE (MD "%TMP%\temp-cach
 REM FIND ALL JPG FILES AND CONVERT THEM TO TEMPORARY CACHE FORMAT
 SETLOCAL ENABLEEXTENSIONS
 FOR %%G IN (*.jpg) DO (
-    FOR /F "TOKENS=1-2" %%H IN ('identify.exe +ping -format "%%w %%h" "%%G"') DO (
+    FOR /F "TOKENS=1-2" %%H IN ('"%TMP%\identify.exe" +ping -format "%%w %%h" "%%G"') DO (
         ECHO Creating: %%~nG.mpc ^+ %%~nG.cache
         ECHO=
-        convert.exe "%%G" -monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip ^
+        "%TMP%\convert.exe" "%%G" -monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip ^
         -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off ^
         -auto-level -enhance -interlace none -colorspace sRGB "%TMP%\temp-cache-files\%%~nG.mpc"
         CLS
@@ -65,7 +56,7 @@ SETLOCAL ENABLEEXTENSIONS
 FOR %%G IN ("%TMP%\temp-cache-files\*.mpc") DO (
     ECHO Converting: %%~nG.cache ^>^> %%~nG.jpg
     ECHO=
-    convert.exe "%%G" -monitor "%%~nG.jpg"
+    "%TMP%\convert.exe" "%%G" -monitor "%%~nG.jpg"
     CLS
 )
 
@@ -73,8 +64,7 @@ FOR %%G IN ("%TMP%\temp-cache-files\*.mpc") DO (
 
 REM CLEANUP TEMP FILES AND FOLDERS
 RD /S /Q %TMP%\temp-cache-files
-DEL /Q convert.exe
-DEL /Q identify.exe
-DEL /Q urls.txt
+DEL /Q "%TMP%\convert.exe"
+DEL /Q "%TMP%\identify.exe"
 START "" /MAX explorer.exe "%CD%"
 START "" /I CMD /D /C DEL /Q optimize.bat
