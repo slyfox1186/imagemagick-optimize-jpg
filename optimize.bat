0d8284d2a20f15fc5aa463464aee7c8ebe3a56cc
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-09 22:03:31 -0400

    Update optimize.bat

--- optimize.bat
+++ optimize.bat
@@ -22,15 +22,17 @@ IF NOT EXIST "%TMP%\temp-cache-files\" MD "%TMP%\temp-cache-files\" >NUL
 IF NOT EXIST "%CD%\optimized\" MD "%CD%\optimized\" >NUL
 IF NOT EXIST "%CD%\originals\" MD "%CD%\originals\" >NUL
 REM MOVE EXE FILES AS WELL
-IF EXIST "%CD%\convert.exe" MOVE /Y "%CD%\convert.exe" "%TMP%\temp-cache-files\" >NUL
-IF EXIST "%CD%\identify.exe" MOVE /Y "%CD%\identify.exe" "%TMP%\temp-cache-files\" >NUL
+IF EXIST "%CD%\convert.exe" MOVE /Y "%CD%\convert.exe" "%TMP%\temp-cache-files\"
+IF EXIST "%CD%\identify.exe" MOVE /Y "%CD%\identify.exe" "%TMP%\temp-cache-files\"
+PAUSE
 
 :----------------------------------------------------------------------------------
 
 REM DELETE FILES FROM ANY PRIOR FAILED ATTEMPTS
 IF EXIST "index.html" DEL /Q "index.html" >NUL
-IF EXIST "%CD%\convert.exe" IF EXIST "%TMP%\temp-cache-files\convert.exe" DEL /Q /F "%CD%\convert.exe" >NUL
-IF EXIST "%CD%\identify.exe" IF EXIST "%TMP%\temp-cache-files\identify.exe" DEL /Q /F "%CD%\identify.exe" >NUL
+IF EXIST "%CD%\convert.exe" IF EXIST "%TMP%\temp-cache-files\convert.exe" DEL "%CD%\convert.exe"
+IF EXIST "%CD%\identify.exe" IF EXIST "%TMP%\temp-cache-files\identify.exe" DEL "%CD%\identify.exe"
+PAUSE
 
 :----------------------------------------------------------------------------------
 
