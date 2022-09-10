d50ef1f19030bc7265cc48d1beffdcac24b7c170
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-09-10 17:57:35 -0400

    Update opt.bat

--- opt.bat
+++ opt.bat
@@ -1,16 +1,11 @@
 @ECHO OFF
-SETLOCAL ENABLEEXTENSIONS
 COLOR 0A
 
 :----------------------------------------------------------------------------------
 
-REM CHANGE DIRECTORY TO THE SCRIPTS DIRECTORY AND OPEN CMD IN MAXIMIZED WINDOW
-PUSHD "%~dp0"
-SET NEW_DIR=%1
-CLS & ECHO=
-ECHO %NEW_DIR%
-PAUSE
-EXIT
+REM CHANGE DIRECTORY TO THE CALLER SCRIPT'S DIRECTORY "run.bat"
+PUSHD "%1"
+
 :----------------------------------------------------------------------------------
 
 REM SET THE TITLE OF THE WINDOW
@@ -18,24 +13,28 @@ FOR %%A IN (.) DO TITLE Optimize jpg images: %%~fA
 
 :----------------------------------------------------------------------------------
 
-REM CREATE AND MOVE FILES IN WINDOWS' TEMP DIRECTORY: "%TMP%"
-REM CREATE THE DIRECTORIES FOR THE CACHE, INPUT, AND OUTPUT FILES
+REM FIND IMAGEMAGICK'S CONVERT.EXE FILE AND SET A VARIABLE TO POINT TO IT'S FULL PATH
+FOR /F "USEBACKQ TOKENS=*" %%A IN (`WHERE convert.exe ^| FINDSTR /I /R ".*ImageMagick.*convert.exe$"`) DO SET "CONVERT="%%A""
+FOR /F "USEBACKQ TOKENS=*" %%A IN (`WHERE identify.exe ^| FINDSTR /I /R ".*ImageMagick.*identify.exe$"`) DO SET "IDENTIFY=""%%A^""
+
+:----------------------------------------------------------------------------------
+
+REM CREATE FILES IN WINDOWS' TEMP DIRECTORY: "%TMP%"
 IF NOT EXIST "%TMP%\jpg-cache\" MD "%TMP%\jpg-cache\" >NUL
 IF NOT EXIST "%CD%\optimized\" MD "%CD%\optimized\" >NUL
 
 :----------------------------------------------------------------------------------
 
 REM FIND ALL JPG FILES AND CONVERT THEM TO TEMP CACHE FORMAT THEN COMBINE THE FILES AND OUTPUT THE OPTIMIZED IMAGES
-REM THIS WILL DELETE THE ORIGIAL FILE AND STORE THE OPTIMIZED IMAGE IN THE FOLDER "OPTIMIZED"
 FOR %%G IN (*.jpg) DO (
-    FOR /F "TOKENS=1-2" %%H IN ('identify.exe +ping -format "%%w %%h" "%%G"') DO (
+    FOR /F "TOKENS=1-2" %%H IN ('%IDENTIFY% +ping -format "%%w %%h" "%%G"') DO (
         CLS & ECHO Creating: %%~nG.mpc ^+ %%~nG.cache
         ECHO=
-        convert.exe "%CD%\%%~nxG" -monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip ^
+        %CONVERT% "%CD%\%%~nxG" -monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip ^
         -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off ^
         -auto-level -enhance -interlace none -colorspace sRGB "%TMP%\jpg-cache\%%~nG.mpc" & CLS
         IF EXIST "%TMP%\jpg-cache\%%~nG.mpc" (
-            convert.exe "%TMP%\jpg-cache\%%~nG.mpc" -monitor "%CD%\optimized\%%~nG.jpg" & CLS
+            %CONVERT% "%TMP%\jpg-cache\%%~nG.mpc" -monitor "%CD%\optimized\%%~nG.jpg" & CLS
             DEL /Q "%TMP%\jpg-cache\%%~nG.cache" "%TMP%\jpg-cache\%%~nG.mpc" "%CD%\%%~nG.jpg"
         )
     )
@@ -43,6 +42,6 @@ FOR %%G IN (*.jpg) DO (
 
 :----------------------------------------------------------------------------------
 
-REM OPEN PARENT FOLDER IN EXPLORER ONCE SCRIPT HAS FINISHED
+REM OPEN PARENT FOLDER IN EXPLORER
 START "" /MAX "%WINDIR%\explorer.exe" "%CD%\"
 EXIT /B
