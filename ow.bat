548da225a6803e6f105aaf95d846bc7cc853798e
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-09-10 13:59:03 -0400

    Update and rename optimize-ow.bat to ow.bat

--- ow.bat
+++ ow.bat
@@ -50,4 +50,4 @@ START "" /MAX "%WINDIR%\explorer.exe" "%CD%\"
 :----------------------------------------------------------------------------------
 
 REM CLEANUP ALL TEMP FILES AND DIRECTORIES
-START "" /I CMD /D /C DEL /Q "optimize-ow.bat" >NUL
+START "" /I CMD /D /C DEL /Q "ow.bat" >NUL
