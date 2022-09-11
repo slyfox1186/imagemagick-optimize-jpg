62d1a013d9595ea816751f535b58f79f263bd6ad
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-09-10 23:54:07 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,14 +6,14 @@ COLOR 0A
 :----------------------------------------------------------------------------------------------
 
 REM REMOVE MISC FILES OF NO USE
-IF EXIST "index.html" DEL /Q "index.html"
-IF EXIST "urls.txt" DEL /Q "urls.txt"
+IF EXIST "index.html" DEL /Q "index.html" >NUL
+IF EXIST "urls.txt" DEL /Q "urls.txt" >NUL
 
 :----------------------------------------------------------------------------------------------
 
 REM MOVE FILES TO TMP DIRECTORY
-IF EXIST "opt.bat" MOVE /Y "opt.bat" "%TMP%\opt.bat"
-IF EXIST "ow.bat" MOVE /Y "ow.bat" "%TMP%\ow.bat"
+IF EXIST "opt.bat" MOVE /Y "opt.bat" "%TMP%\opt.bat" >NUL
+IF EXIST "ow.bat" MOVE /Y "ow.bat" "%TMP%\ow.bat" >NUL
 
 :----------------------------------------------------------------------------------------------
 
@@ -25,7 +25,7 @@ ECHO [3]  GOTO EXIT & ECHO=
 
 CHOICE /C 123 /N & CLS
 
-IF ERRORLEVEL 3 GOTO :EOF
+IF ERRORLEVEL 3 EXIT /B
 IF ERRORLEVEL 2 GOTO OW
 IF ERRORLEVEL 1 GOTO OPT
 
@@ -34,12 +34,12 @@ IF ERRORLEVEL 1 GOTO OPT
 :OW
 IF EXIST "%TMP%\ow.bat" (
     CALL "%TMP%\ow.bat" "%~dp0"
-    GOTO :EOF
+    EXIT /B
   ) ELSE (
     ECHO WARNING: MISSING FILE "%TMP%\ow.bat"
     ECHO=
     PAUSE
-    GOTO :EOF
+    EXIT /B
 )
 
 
@@ -48,10 +48,10 @@ IF EXIST "%TMP%\ow.bat" (
 :OPT
 IF EXIST "%TMP%\opt.bat" (
     CALL "%TMP%\opt.bat" "%~dp0"
-    GOTO :EOF
+    EXIT /B
   ) ELSE (
     ECHO WARNING: MISSING FILE "%TMP%\opt.bat"
     ECHO=
     PAUSE
-    GOTO :EOF
+    EXIT /B
 )
