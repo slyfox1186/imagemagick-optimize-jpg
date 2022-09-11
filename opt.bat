f838d97efe31adf9d8600782093ac7ed0c1e3f33
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-09-11 00:34:13 -0400

    Update opt.bat

--- opt.bat
+++ opt.bat
@@ -20,8 +20,8 @@ FOR /F "USEBACKQ TOKENS=*" %%A IN (`WHERE identify.exe ^| FINDSTR /I /R ".*Image
 :----------------------------------------------------------------------------------
 
 REM CREATE FILES IN WINDOWS' TEMP DIRECTORY: "%TMP%"
-IF NOT EXIST "%TMP%\jpg-cache\" MD "%TMP%\jpg-cache\" >NUL
-IF NOT EXIST "%CD%\optimized\" MD "%CD%\optimized\" >NUL
+IF NOT EXIST "%TMP%\jpg-cache\" MD "%TMP%\jpg-cache\" 2>NUL
+IF NOT EXIST "optimized\" MD "optimized\" 2>NUL
 
 :----------------------------------------------------------------------------------
 
@@ -30,12 +30,12 @@ FOR %%G IN (*.jpg) DO (
     FOR /F "TOKENS=1-2" %%H IN ('%IDENTIFY% +ping -format "%%w %%h" "%%G"') DO (
         CLS & ECHO Creating: %%~nG.mpc ^+ %%~nG.cache
         ECHO=
-        %CONVERT% "%CD%\%%~nxG" -monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip ^
+        %CONVERT% "%%~nxG" -monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip ^
         -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off ^
         -auto-level -enhance -interlace none -colorspace sRGB "%TMP%\jpg-cache\%%~nG.mpc" & CLS
         IF EXIST "%TMP%\jpg-cache\%%~nG.mpc" (
-            %CONVERT% "%TMP%\jpg-cache\%%~nG.mpc" -monitor "%CD%\optimized\%%~nG.jpg" & CLS
-            DEL /Q "%TMP%\jpg-cache\%%~nG.cache" "%TMP%\jpg-cache\%%~nG.mpc" "%CD%\%%~nG.jpg"
+            %CONVERT% "%TMP%\jpg-cache\%%~nG.mpc" -monitor "optimized\%%~nG.jpg" & CLS
+            DEL /Q "%TMP%\jpg-cache\%%~nG.cache" "%TMP%\jpg-cache\%%~nG.mpc"
         )
     )
 )
