e0d4c6241a622fc5a84f28868b30c427b05bda30
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-09-12 17:41:56 -0400

    Update ow.bat

--- ow.bat
+++ ow.bat
@@ -9,8 +9,8 @@ PUSHD "%1"
 :----------------------------------------------------------------------------------
 
 REM FIND IMAGEMAGICK'S CONVERT.EXE FILE AND SET A VARIABLE TO POINT TO IT'S FULL PATH
-FOR /F "USEBACKQ TOKENS=*" %%A IN (`WHERE convert.exe ^| FINDSTR /I /R ".*ImageMagick.*convert.exe$"`) DO SET "CONVERT="%%A""
-FOR /F "USEBACKQ TOKENS=*" %%B IN (`WHERE identify.exe ^| FINDSTR /I /R ".*ImageMagick.*identify.exe$"`) DO SET "IDENTIFY=""%%B^""
+FOR /F "USEBACKQ TOKENS=*" %%A IN (`WHERE convert.exe ^| FINDSTR /I /R ".*ImageMagick.*convert.exe$"`) DO SET "CONVERT=%%A"
+FOR /F "USEBACKQ TOKENS=*" %%B IN (`WHERE identify.exe ^| FINDSTR /I /R ".*ImageMagick.*identify.exe$"`) DO SET "IDENTIFY=%%B"
 REM "IDENTIFY=""%%A^""
 
 :----------------------------------------------------------------------------------
@@ -23,14 +23,14 @@ IF NOT EXIST "%TMP%\jpg-cache\" MD "%TMP%\jpg-cache\" >NUL
 
 REM FIND ALL JPG FILES AND CONVERT THEM TO TEMP CACHE FORMAT THEN COMBINE THE FILES AND OUTPUT THE OPTIMIZED IMAGES
 FOR %%G IN (*.jpg) DO (
-    FOR /F "TOKENS=1-2" %%H IN ('%IDENTIFY% +ping -format "%%w %%h" "%%G"') DO (
+    FOR /F "TOKENS=1-2" %%H IN ('""%IDENTIFY%^" +ping -format "%%w %%h" "%%G"') DO (
         CLS & ECHO Creating: %%~nG.mpc ^+ %%~nG.cache
         ECHO=
-        %CONVERT% "%CD%\%%~nxG" -monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip ^
+        "%CONVERT%" "%CD%\%%~nxG" -monitor -filter Triangle -define filter:support=2 -thumbnail "%%Hx%%I" -strip ^
         -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off ^
         -auto-level -enhance -interlace none -colorspace sRGB "%TMP%\jpg-cache\%%~nG.mpc" & CLS
         IF EXIST "%TMP%\jpg-cache\%%~nG.mpc" (
-            %CONVERT% "%TMP%\jpg-cache\%%~nG.mpc" -monitor "%CD%\%%~nG.jpg" & CLS
+            "%CONVERT%" "%TMP%\jpg-cache\%%~nG.mpc" -monitor "%CD%\%%~nG.jpg" & CLS
             DEL /Q "%TMP%\jpg-cache\%%~nG.cache" "%TMP%\jpg-cache\%%~nG.mpc" >NUL
         )
     )
@@ -40,6 +40,4 @@ FOR %%G IN (*.jpg) DO (
 
 REM OPEN PARENT FOLDER IN EXPLORER
 REM START "" /MAX "%WINDIR%\explorer.exe" "%CD%"
-ECHO=
-PAUSE
 EXIT /B
