698dafec020bf499a4dd34103dc9dfbed86a4f85
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-11-10 14:10:51 -0500

    Update ow.sh

--- ow.sh
+++ ow.sh
@@ -13,12 +13,12 @@ for i in *.jpg; do
     dimension="$(identify -format '%wx%h' "$i")"
     convert "$i" -monitor -filter Triangle -define filter:support=2 -thumbnail $dimension -strip \
     -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off \
-    -auto-level -enhance -interlace none -colorspace sRGB "$(basename -- "$i" .jpg).mpc"
+    -auto-level -enhance -interlace none -colorspace sRGB "${i%%.jpg}.mpc"
     clear
     for i in *.mpc; do
         if [ -f "$i" ]; then
             echo -e "\\nConverting: $i\\n"
-            convert "$i" -monitor "$(basename -- "$i" .mpc).jpg"
+            convert "$i" -monitor "${i%%.mpc}.jpg"
             rm *.cache
             rm *.mpc
             clear
