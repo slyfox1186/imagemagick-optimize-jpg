375c00b10f3ed3159741038b234f221f42968cb7
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-11-09 19:33:07 -0500

    Update ow.sh

--- ow.sh
+++ ow.sh
@@ -2,9 +2,12 @@
 
 clear
 
-# finds all jpg files and converts them to a .cache and .mpc cache
-# format. then combines both files and outputs a highly optimized
-# overwritten verson of the original images.
+echo 'finds all jpg files and converts them to a .cache and .mpc cache.'
+echo 'format. then combines both files and outputs a highly optimized'
+echo 'overwritten verson of the original images.'
+echo
+read -p 'Press Enter to confirm that you understand this will overwrite the original files!'
+clear
 for i in *.jpg; do
     echo -e "\\nCreating cache files: .mpc + .cache\\n"
     dimension="$(identify -format '%wx%h' "$i")"
