4b46ab1a00e7244326f0863c15f5e675d9a5dc73
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-11-10 21:30:27 -0500

    Update ow.sh

--- ow.sh
+++ ow.sh
@@ -8,20 +8,28 @@ echo 'files and outputs a highly optimized (AND OVERWRITTEN) verson of the origi
 echo
 read -p 'Press [Enter] to CONFIRM that you UNDERSTAND this script WILL OVERWRITE the ORIGINAL FILES!'
 clear
+
+if [ ! -d output ]; then mkdir -p output; fi
+
+PARENT_DIR="$(echo "$PWD")"
+
 for i in *.jpg; do
     echo -e "\\nCreating two temporary cache files: ${i%%.jpg}.mpc + ${i%%.jpg}.cache\\n"
     dimension="$(identify -format '%wx%h' "$i")"
     convert "$i" -monitor -filter Triangle -define filter:support=2 -thumbnail $dimension -strip \
     -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off \
-    -auto-level -enhance -interlace none -colorspace sRGB "${i%%.jpg}.mpc"
+    -auto-level -enhance -interlace none -colorspace sRGB "/tmp/${i%%.jpg}.mpc"
     clear
-    for i in *.mpc; do
+    for i in /tmp/*.mpc; do
         if [ -f "$i" ]; then
             echo -e "\\nOverwriting orignal file with optimized self: $i >> ${i%%.mpc}.jpg\\n"
             convert "$i" -monitor "${i%%.mpc}.jpg"
-            rm *.cache
-            rm *.mpc
-            clear
+            if [ -f "${i%%.mpc}.jpg" ]; then
+                mv "${i%%.mpc}.jpg" "$PARENT_DIR"
+                rm "${i%%.mpc}.cache"
+                rm "$i"
+                clear
+            fi
         fi
     done
 done
