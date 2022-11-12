27613c5c76977462127288c4f50410d35e64525f
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-11-12 01:33:57 -0500

    Update ow.sh

--- ow.sh
+++ ow.sh
@@ -2,6 +2,7 @@
 
 clear
 
+# explain purpose of script to user
 echo "[i] This script locates and stores the paths of each jpg file found in the script's directory, and"
 echo '    from each match it finds and creates a set of temporary cache files.'
 echo
@@ -13,12 +14,14 @@ echo
 read -p "[i] Please enter 'Yes' to continue the scripts execution: " uChoice
 clear
 
+# validate the user's input
 if [[ "$uChoice" != "Yes" ]]; then
     echo -e "[i] You must enter Yes 'exactly as shown' to continue the scripts execution.\\n"
     read -p '[i] Press Enter to start over or ^Z to exit.'
     bash "$0"
     exit 1
 elif [[ "$uChoice" = "Yes" ]]; then
+    # find all jpg files and create temporary cache files from them
     for i in *.jpg; do
         echo -e "\\nCreating two temporary cache files: ${i%%.jpg}.mpc + ${i%%.jpg}.cache\\n"
         dimension="$(identify -format '%wx%h' "$i")"
@@ -27,13 +30,17 @@ elif [[ "$uChoice" = "Yes" ]]; then
         -auto-level -enhance -interlace none -colorspace sRGB "/tmp/${i%%.jpg}.mpc"
         clear
         for i in /tmp/*.mpc; do
+        # find the temporary cache files created above and output optimized jpg files
             if [ -f "$i" ]; then
                 echo -e "\\nOverwriting orignal file with optimized self: $i >> ${i%%.mpc}.jpg\\n"
                 convert "$i" -monitor "${i%%.mpc}.jpg"
+                # overwrite the original image with it's optimized version
+                # by moving it from the tmp directory to the source directory
                 if [ -f "${i%%.mpc}.jpg" ]; then
                     mv "${i%%.mpc}.jpg" "$PWD"
-                    rm "${i%%.mpc}.cache"
+                    # delete both cache files before continuing
                     rm "$i"
+                    rm "${i%%.mpc}.cache"
                     clear
                 fi
             fi
@@ -41,4 +48,12 @@ elif [[ "$uChoice" = "Yes" ]]; then
     done
 fi
 
+echo -e "\\n[i] The script has reached it's end.\\n"
+read -p '[i] Press Enter to exit'
+clear
+
+# unset variables
 unset dimension
+
+# delete this script from pc
+if [ -f "$0" ]; then rm -- "$0"; fi
