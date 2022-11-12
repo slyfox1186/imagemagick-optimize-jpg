4c1c1755f9dd4166e0389a377d300067b626c306
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-11-11 22:17:29 -0500

    Update ow.sh

--- ow.sh
+++ ow.sh
@@ -2,34 +2,44 @@
 
 clear
 
-echo 'finds all jpg files and converts them to a set of temporary cache'
-echo 'files (file-name.cache and file-name.mpc). It then combines both'
-echo 'files and outputs a highly optimized (AND OVERWRITTEN) verson of the original images.'
+echo "[i] This script locates and stores the paths of each jpg file found in the script's directory, and"
+echo '    from each match it finds and creates a set of temporary cache files.'
 echo
-read -p 'Press [Enter] to CONFIRM that you UNDERSTAND this script WILL OVERWRITE the ORIGINAL FILES!'
+echo '[i] ImageMagick will then utilize each cache file created by running an advanced algorithm that combines'
+echo '    and compiles a highly optimized version and then stores the output by overwriting the original image.'
+echo
+echo '[i] To continue the scripts execution you must confirm that you understand it WILL OVERWRITE THE ORIGINAL FILES!'
+echo
+read -p "Please enter 'Yes' to confirm overwrite: " uChoice
 clear
 
-PARENT_DIR="$(echo "$PWD")"
+if [[ "$uChoice" != "Yes" ]]; then
+    echo -e "[i] You must enter Yes 'exactly as shown' to continue the scripts execution.\\n"
+    read -p '[i] Press [Enter] to start over or [^C] or [^Z] to exit terminal.'
+    bash "$0"
+    exit 1
+elif [[ "$uChoice" = "Yes" ]]; then
 
-for i in *.jpg; do
-    echo -e "\\nCreating two temporary cache files: ${i%%.jpg}.mpc + ${i%%.jpg}.cache\\n"
-    dimension="$(identify -format '%wx%h' "$i")"
-    convert "$i" -monitor -filter Triangle -define filter:support=2 -thumbnail $dimension -strip \
-    -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off \
-    -auto-level -enhance -interlace none -colorspace sRGB "/tmp/${i%%.jpg}.mpc"
-    clear
-    for i in /tmp/*.mpc; do
-        if [ -f "$i" ]; then
-            echo -e "\\nOverwriting orignal file with optimized self: $i >> ${i%%.mpc}.jpg\\n"
-            convert "$i" -monitor "${i%%.mpc}.jpg"
-            if [ -f "${i%%.mpc}.jpg" ]; then
-                mv "${i%%.mpc}.jpg" "$PARENT_DIR"
-                rm "${i%%.mpc}.cache"
-                rm "$i"
-                clear
+    for i in *.jpg; do
+        echo -e "\\nCreating two temporary cache files: ${i%%.jpg}.mpc + ${i%%.jpg}.cache\\n"
+        dimension="$(identify -format '%wx%h' "$i")"
+        convert "$i" -monitor -filter Triangle -define filter:support=2 -thumbnail $dimension -strip \
+        -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off \
+        -auto-level -enhance -interlace none -colorspace sRGB "/tmp/${i%%.jpg}.mpc"
+        clear
+        for i in /tmp/*.mpc; do
+            if [ -f "$i" ]; then
+                echo -e "\\nOverwriting orignal file with optimized self: $i >> ${i%%.mpc}.jpg\\n"
+                convert "$i" -monitor "${i%%.mpc}.jpg"
+                if [ -f "${i%%.mpc}.jpg" ]; then
+                    mv "${i%%.mpc}.jpg" "$PWD"
+                    rm "${i%%.mpc}.cache"
+                    rm "$i"
+                    clear
+                fi
             fi
-        fi
+        done
     done
-done
+fi
 
 unset dimension
