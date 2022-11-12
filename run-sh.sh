8390bc4d8fa9e683fbc18bffa1a8e9bc2cfdd8d5
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-11-12 12:08:22 -0500

    Update run-sh.sh

--- run-sh.sh
+++ run-sh.sh
@@ -10,14 +10,14 @@ if [ -f o.sh ]; then mv -f o.sh /tmp; fi
 if [ -f ow.sh ]; then mv -f ow.sh /tmp; fi
 
 clear
-echo -e "[i] User input required regarding the original files\\n"
+echo -e "[i] User input required:\\n"
 echo '[1] Overwrite orignal files'
 echo -e "[2] Do not overwrite original files\\n"
-read -p 'To make a choice please press [1] or [2]: ' uChoice
+read -p 'Press [1] or [2] to make a selection: ' uChoice
 clear
 
-if [ "$uChoice" = "1" ]; then source /tmp/ow.sh
-elif [ "$uChoice" = "2" ]; then source /tmp/o.sh
+if [ "$uChoice" = "1" ]; then . /tmp/ow.sh
+elif [ "$uChoice" = "2" ]; then . /tmp/o.sh
 else
     echo -e "You must press either the number 1 or 2 key.\\n"
     read -p 'Press Enter to start over.'
@@ -26,5 +26,7 @@ else
     exit 1
 fi
 
-# delete this script from pc
-# if [ -f "$0" ]; then rm -- "$0"; fi
+# delete all scripts from pc
+if [ -f /tmp/o.sh ]; then rm /tmp/o.sh ; fi
+if [ -f /tmp/ow.sh ]; then rm /tmp/ow.sh; fi
+if [ -f "$0" ]; then rm -- "$0"; fi
